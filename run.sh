d2fde5038ac2d9063681919bd314cf292352c35e
SteelAtlas <sawzeno57@outlook.com>
2023-07-19 10:54:44 +0530

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,49 @@
 #! /bin/bash
 
 BUILD_DIR="build"
-
+SCRIPT_DIR=$(cd " $(dirname "$0")")
+ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 if [[ "$1" == "-c" ]];then
 
   echo "RUNNING CMAKE";
 
-  if [[ -d "4BUILD_DIR" ]];then
-    echo "BUILD DIRECTORY EXISTS"
-    cd "$BUILD_DIR"  
-  else
-    echo "BUILD DIRECTORY DOES NOT EXIST ... CREATING "
-    mkdir "$BUILD_DIR"
-    cd "$BUILD_DIR" 
+  if [[ "$SCRIPT_DIR" == "$ROOT_DIR" ]];then
+     BUILD_DIR="$ROOT_DIR/build/power"
+
+    if [[ -d "BUILD_DIR" ]];then
+      echo "BUILD DIRECTORY EXISTS"
+      cd "$BUILD_DIR"  
+    else
+      echo "BUILD DIRECTORY DOES NOT EXIST ... CREATING "
+      mkdir "$BUILD_DIR"
+      cd "$BUILD_DIR" 
+    fi
   fi
 
   cd "$(dirname "$0")/build"
   cmake ..
   make 
 
-  cd "power"
-  ./main
+  if [[ -d "power" ]];then
+    echo "POWER EXISTS"
+    cd "power"   
+    ls
+    ./main
+  fi
   
 elif [[ "$1" == "-b" ]]; then
 
   echo "RUNNING BUILD";
-  cd "$(dirname "$0")/build"
+  echo "current location $(pwd)"
+  cd "$(dirname "$0")/build" 
+  echo "current location $(pwd)"
+
   make
-  cd "power"
+
+
+
+  cd "$(dirname "$0")/build/power"
   ./main
 
 else
